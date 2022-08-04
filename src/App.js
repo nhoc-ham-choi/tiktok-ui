0bf07b75a2ef63f5b4a7052956f3bc881214eabe
nhochamplay <nhochamchoi13042001@gmail.com>
2022-08-05 02:06:41 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {publicRoutes} from '~/routes';
+import {DefaultLayout} from '~/components/Layout';
+import {Fragment} from 'react';
 
 function App() {
   return (
@@ -7,13 +9,31 @@ function App() {
       <div className="App">
         <Routes>
             {publicRoutes.map((route, index) =>{
-              const Page = route.component
-              return <Route key={index} path= {route.path} element={<Page/>}/>
-            })}
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+              const Page = route.component;
+
+                let Layout = DefaultLayout;
+
+                if (route.layout) {
+                  Layout = route.layout
+                } else if (route.layout === null) {
+                  Layout = Fragment;
+                }
+                
+                return (<Route 
+                  key={index} 
+                  path= {route.path} 
+                  element={
+                  <Layout>
+                    <Page/>
+                  </Layout>
+                }
+                />
+                );
+              })}
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 
 export default App;
