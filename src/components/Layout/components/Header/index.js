42e227e9ec7943ad802bd69367cb4cb2f07084d8
nhochamplay <nhochamchoi13042001@gmail.com>
2022-08-08 10:25:38 +0700

    layout for mid

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import styles from './Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +11,19 @@ function Header() {
     return (
     <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-            {/* Logo */}
-            {/* Search */}
+                <img src={images.logo} alt="Tiktok" />
+            <div className={cx('search')}>
+                <input placeholder='Search accounts' spellCheck={false} />
+                <button className={cx('clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx('actions')}></div>
         </div>
     </header>
     );
